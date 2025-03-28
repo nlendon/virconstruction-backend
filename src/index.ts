a5bc673e3df2c55605c47810333416388828deac
nLendon <86872522+nlendon@users.noreply.github.com>
2025-03-28 21:04:38 +0400

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -6,9 +6,16 @@ const Main = async (): Promise<void> => {
   try {
     await Database.authenticate();
     await Database.sync({ force: false });
-    App.listen(process.env.PORT, (): void => console.log(`Server started on port ${process.env.PORT}`));
+    
+    App.listen(
+      process.env.PORT,
+      process.env.HOST,
+      (): void => {
+        console.log(`Server started on http://${process.env.HOST}:${process.env.PORT}`);
+      }
+    );
   } catch (e) {
-    console.log(`error: ${e}` as string);
+    console.log(`error: ${e}`);
   }
 };
 
