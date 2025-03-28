633c5dfc1e5da7da4080285fd6dc1aea31c294bb
nLendon <narimanyanvah@gmail.com>
2025-03-28 22:00:45 +0400

    SSL, https backend

--- src/index.ts
+++ src/index.ts
@@ -9,16 +9,14 @@ const Main = async (): Promise<void> => {
     await Database.authenticate();
     await Database.sync({ force: false });
 
-    // Пути к сертификату и ключу
     const options = {
-      cert: fs.readFileSync('../../../ssl/certs/api_virconstruction_narimanyan_info_d4370_40567_1750931642_d12e280b6e09eaf6ca0c5b941280c268.crt'),
-      key: fs.readFileSync('../../../ssl/private/d4370_40567_1803b5ea92dca98ea3cab10246bcbe8a.key'), // Укажи правильный путь к ключу
+      cert: fs.readFileSync('../ssl/api-vircon-sert.crt'),
+      key: fs.readFileSync('../ssl/api-vircon-keey.key'),
     };
 
-    // Запускаем HTTPS-сервер
     https.createServer(options, App).listen(
-      parseInt(process.env.PORT!), 
-      process.env.HOST!, 
+      parseInt(process.env.PORT!),
+      process.env.HOST!,
       () => {
         console.log(`Server started on https://${process.env.HOST}:${process.env.PORT}`);
       }
--- src/schema/global.schema.ts
+++ src/schema/global.schema.ts
@@ -5,7 +5,7 @@ export const GlobalUpdateSchema = object({
     name: z.enum(['workers', 'clients', 'com_projects', 'run_projects']),
     count: z.number({ invalid_type_error: 'Invalid type of Count' })
       .min(0, 'Count must be greater than 0')
-      .max(1000, 'Count must be less than 0').optional(),
+      .max(1000, 'Count must be less than 1000').optional(),
     value: z.boolean({ invalid_type_error: 'Invalid type of value' }).optional(),
   }),
 });
